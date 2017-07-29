0dd5a45b68a0ac51d62c965843f001bb9460574f
Frantisek Vesely <vesely@grandmedia.z>
2017-07-29 23:05:04 +0200

    Add FullyQualifiedFunctionSniff

---
+++ tests/Sniffs/Namespaces/data/invalid.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace GrandMediaCodingStandard\Foo;
+
+use DateTime;
+
+function bar(): void
+{
+}
+
+final class Foo
+{
+
+	public function foo(int $number): void
+	{
+		strlen('foo');
+		DateTime::createFromFormat(strlen('foo'), '');
+		$this->foo(strlen('foo'));
+		bar();
+	}
+
+}
